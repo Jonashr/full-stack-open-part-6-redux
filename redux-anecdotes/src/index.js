947b448f7dcddccb41bf72cac1103031011ff0d6
Jonas Horvei <jonashorvei1@gmail.com>
2019-10-07 13:33:10 +0200

    Refactored store to its own component

--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -1,32 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, combineReducers } from 'redux'
-import anecdoteService from './services/anecdotes'
 import { Provider } from 'react-redux'
 import App from './App'
-import notificationReducer from './reducers/notificationReducer'
-import anecdoteReducer, {initializeAnecdotes} from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
+import store from './store'
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  notification: notificationReducer,
-  filter: filterReducer
-})
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 
-const store = createStore(reducer)
 
-anecdoteService.getAll().then(anecdotes => 
-  store.dispatch(initializeAnecdotes(anecdotes)))
-
-const render = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root')
-  )
-}
-
-render()
-store.subscribe(render)
